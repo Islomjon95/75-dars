7a5826dce3114672bc1026bf780711b89a095d34
Islom_dev <inogomovislom@gmail.com>
2023-05-17 14:41:31 +0500

    Rest va trenary operator

--- main.js
+++ main.js
@@ -13,3 +13,32 @@
 
 // addNumber()
 //________________________________
+// "for of" array of students
+// const students = [
+//     {name: "Javohir", age: 16},
+//     {name: "Aziz", age: 17},
+//     {name: "Behruz", age: 16}
+// ]
+
+// for( let student of students){
+//     console.log(`${student.name} yoshi ${student.age} da`)
+// }
+
+
+
+// ________________________________________
+//Rest oprator
+// function number (...num){
+//     for(let numb of num){
+//         // numb<30 ? console.log(numb) : console.log() trenary
+
+//         if(numb<30){
+//             console.log(numb)
+//         }
+//     }
+// }
+
+// number(45, 54, 21, 12)
+
+
+
