6e112f8400347773f98ef9253e0365ec876fa34e
Islom_dev <inogomovislom@gmail.com>
2023-05-17 14:30:00 +0500

    Default params

---
+++ main.js
@@ -0,0 +1,15 @@
+//Default params
+
+// function coder (name="Shaxnoza"){
+//     console.log(`${name} the best coder`)
+
+// }
+
+// coder("Odina")
+
+// function addNumber (numb1=0, numb2=0){
+//     console.log(numb1+numb2)
+// }
+
+// addNumber()
+//________________________________
