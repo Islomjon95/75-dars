7d75b810fa2379d9afd3b674285344ed11e0b36c
Islom_dev <inogomovislom@gmail.com>
2023-05-17 16:10:36 +0500

    Chart.js

--- main.js
+++ main.js
@@ -48,9 +48,90 @@
 
 // console.log(arr.includes(100))
 
-const group8 = ["Aziz", "Bahodir", "Jahongir", "Farrux", "Behruz", "Shaxnoza", "Odina", "Diyorbek", "Javohir"]
+// const group8 = ["Aziz", "Bahodir", "Jahongir", "Farrux", "Behruz", "Shaxnoza", "Odina", "Diyorbek", "Javohir"]
 
-const name = "Odina"
+// const name = "Odina"
 
-group8.includes(name) ? console.log(`${name} bugun darsga qatnashdi`) : console.log(`${name} bugun darsga qatnashmadi`)
+// group8.includes(name) ? console.log(`${name} bugun darsga qatnashdi`) : console.log(`${name} bugun darsga qatnashmadi`)
+// ________________________________________
+// Class constructor
+
+// class car {
+//     constructor(model, year){
+//         this.model = model,
+//         this.year = year
+//     }
+// }
+
+// const car1 = {
+//     model: "Nexia 3",
+//     year: 2020
+// }
+
+// const car2 = new car ("Captiva", 2017)
+
+// console.log(car1, car2)
+
+// __________________________________________
+
+// Chart.JS
+
+// setInterval(()=>{
+
+//     let a = Math.floor(Math.random()*100)
+//     let b = Math.floor(Math.random()*100)
+//     let c = Math.floor(Math.random()*100)
+//     let d = Math.floor(Math.random()*100)
+//     let f = Math.floor(Math.random()*100)
+
+//     numb(a,b,c,d,f)
+// },1000)
+
+
+
+
+
+const ctx = document.getElementById('myChart');
+
+     play = setInterval(()=>{
+        let a = Math.floor(Math.random()*100)
+        let b = Math.floor(Math.random()*100)
+        let c = Math.floor(Math.random()*100)
+        let d = Math.floor(Math.random()*100)
+        let f = Math.floor(Math.random()*100)
+    
+        arr = []
+        arr.push(a,b,c,d,f)
+        return arr
+        
+    },1000)
+
+
+
+
+
+new Chart(ctx, {
+    
+  type: 'polarArea',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [...play],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+
+
+
 
